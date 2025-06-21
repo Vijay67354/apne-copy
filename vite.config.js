dec7a2c484b64acc22b87dbe851a1ec3cd221f28
vijay Thakur <practice66375@gmail.com>
2025-06-22 02:44:28 +0530

    sd

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5006',
+        target: 'https://apne-copy.vercel.app',
         changeOrigin: true,
         secure: false,
       },
